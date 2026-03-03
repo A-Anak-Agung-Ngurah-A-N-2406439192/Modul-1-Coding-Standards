fca4b3c4b8fb543ddbb149cadfc29d3bf4a59789
abhivadya1 <abhivadya1@gmail.com>
2026-03-03 12:01:21 +0700

    fix coverage test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
@@ -0,0 +1,90 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CarController.class)
+class CarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductService productService;
+
+    @MockitoBean
+    private CarServiceImpl carService;
+
+    @Test
+    void testCreateCarPage() throws Exception {
+        mockMvc.perform(get("/car/createCar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createCar"))
+                .andExpect(model().attributeExists("car"));
+    }
+
+    @Test
+    void testCreateCarPost() throws Exception {
+        Car car = new Car();
+        car.setCarId("123");
+        when(carService.create(any(Car.class))).thenReturn(car);
+
+        mockMvc.perform(post("/car/createCar")
+                        .flashAttr("car", car))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("listCar"));
+    }
+
+    @Test
+    void testCarListPage() throws Exception {
+        when(carService.findAll()).thenReturn(Arrays.asList(new Car()));
+
+        mockMvc.perform(get("/car/listCar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("carList"))
+                .andExpect(model().attributeExists("cars"));
+    }
+
+    @Test
+    void testEditCarPage() throws Exception {
+        Car car = new Car();
+        when(carService.findById("123")).thenReturn(car);
+
+        mockMvc.perform(get("/car/editCar/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("editCar"))
+                .andExpect(model().attributeExists("car"));
+    }
+
+    @Test
+    void testEditCarPost() throws Exception {
+        Car car = new Car();
+        car.setCarId("123");
+
+        mockMvc.perform(post("/car/editCar")
+                        .flashAttr("car", car))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("listCar"));
+    }
+
+    @Test
+    void testDeleteCar() throws Exception {
+        mockMvc.perform(post("/car/deleteCar")
+                        .param("carId", "123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("listCar"));
+    }
+}
