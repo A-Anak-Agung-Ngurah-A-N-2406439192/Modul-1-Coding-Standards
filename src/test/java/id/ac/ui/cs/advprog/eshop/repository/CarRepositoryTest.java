89e77a43032481ad1100bef9aa2e12578adbd329
abhivadya1 <abhivadya1@gmail.com>
2026-03-03 11:42:24 +0700

    testing for car, carrepository, carserviceimpl

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryTest.java
@@ -0,0 +1,97 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ExtendWith(MockitoExtension.class)
+public class CarRepositoryTest {
+    @InjectMocks
+    CarRepository carRepository;
+
+    @BeforeEach
+    void setUp(){
+    }
+
+    @Test
+    void testCreateAndFind(){
+        Car car = new Car();
+        car.setCarId("testcarid");
+        car.setCarColor("white");
+        car.setCarName("Lamborghini");
+        car.setCarQuantity(100);
+        carRepository.create(car);
+
+        Iterator<Car> carIterator = carRepository.findAll();
+        assertTrue(carIterator.hasNext());
+        Car savedCar = carIterator.next();
+        assertEquals(car.getCarId(), savedCar.getCarId());
+        assertEquals(car.getCarColor(), savedCar.getCarColor());
+        assertEquals(car.getCarName(), savedCar.getCarName());
+        assertEquals(car.getCarQuantity(), savedCar.getCarQuantity());
+    }
+
+    @Test
+    void testFindIdFound(){
+        Car car = new Car();
+        car.setCarId("testcarid");
+        car.setCarColor("white");
+        car.setCarName("Lamborghini");
+        car.setCarQuantity(100);
+        carRepository.create(car);
+
+        Car foundCar = carRepository.findById(car.getCarId());
+        assertNotNull(foundCar);
+        assertEquals(car.getCarName(), foundCar.getCarName());
+    }
+
+    @Test
+    void testFindIdNotFound(){
+        Car foundCar = carRepository.findById("apappa");
+        assertNull(foundCar);
+    }
+
+    @Test
+    void testUpdateCar(){
+        Car car = new Car();
+        car.setCarColor("white");
+        car.setCarName("Lamborghini");
+        car.setCarQuantity(100);
+        carRepository.create(car);
+
+        Car updatedCar = new Car();
+        car.setCarColor("black");
+        car.setCarName("Ferrari");
+        car.setCarQuantity(10);
+
+        Car result = carRepository.update(car.getCarId(), updatedCar);
+
+        assertNotNull(result);
+        assertEquals("black", result.getCarColor());
+        assertEquals("Ferrari", result.getCarName());
+        assertEquals(10, result.getCarQuantity());
+    }
+
+    @Test
+    void testDeleteCar(){
+        Car car = new Car();
+        car.setCarColor("white");
+        car.setCarName("Lamborghini");
+        car.setCarQuantity(100);
+        carRepository.create(car);
+
+        carRepository.delete(car.getCarId());
+
+        Car foundCar = carRepository.findById(car.getCarId());
+        assertNull(foundCar);
+    }
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImplTest.java
@@ -0,0 +1,76 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+public class CarServiceImplTest {
+    @Mock
+    CarRepository carRepository;
+
+    @InjectMocks
+    CarServiceImpl carService;
+    Car car;
+
+    @BeforeEach
+    void setUp(){
+        Car car = new Car();
+        car.setCarId("testcarid");
+        car.setCarColor("white");
+        car.setCarName("Lamborghini");
+        car.setCarQuantity(100);
+    }
+
+    @Test
+    void testCreate(){
+        when(carRepository.create(car)).thenReturn(car);
+        Car createdCar = carService.create(car);
+        assertEquals(car.getCarId(), createdCar.getCarId());
+        verify(carRepository, times(1)).create(car);
+    }
+
+    @Test
+    void testFindAll(){
+        List<Car> carList = new ArrayList<>();
+        carList.add(car);
+        when(carRepository.findAll()).thenReturn(carList.iterator());
+
+        List<Car> foundCar = carService.findAll();
+        assertFalse(foundCar.isEmpty());
+        assertEquals(1, foundCar.size());
+        verify(carRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testFindById(){
+        when(carRepository.findById("testcarid")).thenReturn(car);
+        Car findCar = carService.findById(car.getCarId());
+        assertNotNull(findCar);
+        assertEquals("Lamborghini", findCar.getCarName());
+        verify(carRepository, times(1)).findById("testidcar");
+    }
+
+    @Test
+    void testUpdate(){
+        Car updateCar = new Car();
+        updateCar.setCarName("ulala");
+        carService.update("testcarid", updateCar);
+        verify(carRepository, times(1)).update("testcarid",updateCar);
+    }
+
+    @Test
+    void testDeleteCar(){
+        carService.deleteCarById("testcarid");
+        verify(carRepository, times(1)).delete("testcarid");
+    }
+}
