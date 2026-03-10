9fefb68bf3de30f0d66be72868a8041a9fb3f10b
abhivadya1 <abhivadya1@gmail.com>
2026-03-10 17:09:02 +0700

    fix import in OrderRepositoryTest

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,12 +1,10 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Builder
