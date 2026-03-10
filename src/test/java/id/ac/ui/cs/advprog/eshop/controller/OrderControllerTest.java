3de035124a1cf57412fe5260c89ba81976a7edb6
abhivadya1 <abhivadya1@gmail.com>
2026-03-10 19:24:54 +0700

    [RED] Add functional tests for OrderController

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -0,0 +1,63 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = OrderController.class)
+class OrderControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private OrderService orderService;
+
+    @Test
+    void testCreateOrderPage() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/createOrder"));
+    }
+
+    @Test
+    void testHistoryOrderPage() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/historyForm"));
+    }
+
+    @Test
+    void testHistoryOrderPost() throws Exception {
+        when(orderService.findAllByAuthor(anyString())).thenReturn(new ArrayList<>());
+        mockMvc.perform(post("/order/history").param("author", "Safira"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/historyList"))
+                .andExpect(model().attributeExists("orders"));
+    }
+
+    @Test
+    void testPayOrderPage() throws Exception {
+        mockMvc.perform(get("/order/pay/order-1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/payOrder"));
+    }
+
+    @Test
+    void testPayOrderPost() throws Exception {
+        mockMvc.perform(post("/order/pay/order-1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("order/paySuccess"));
+    }
+}
