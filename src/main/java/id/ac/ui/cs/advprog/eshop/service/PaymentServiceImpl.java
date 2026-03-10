41f2d8b4da0523b257ba78d1221690b06391016d
abhivadya1 <abhivadya1@gmail.com>
2026-03-10 18:56:29 +0700

    [GREEN] Implement PaymentService features

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,48 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        String paymentId = UUID.randomUUID().toString();
+        Payment payment = new Payment(paymentId, method, paymentData, order);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        Payment p = paymentRepository.findById(payment.getId());
+        if (p != null) {
+            p.setStatus(status);
+            if ("SUCCESS".equals(status)) {
+                p.getOrder().setStatus("SUCCESS");
+            } else if ("REJECTED".equals(status)) {
+                p.getOrder().setStatus("FAILED");
+            }
+            paymentRepository.save(p);
+        }
+        return p;
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
+
+    @Override
+    public List<Payment> getAllPayment() {
+        return paymentRepository.findAll();
+    }
+}
